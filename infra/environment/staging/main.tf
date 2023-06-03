4aaa14a60075ee92a89b1472a773ab2c50f458da
bwkw <na.shota.1139@gmail.com>
2023-06-02 20:12:22 -0700

    :shirt: Format

--- infra/environment/staging/main.tf
+++ infra/environment/staging/main.tf
@@ -1,17 +1,17 @@
 locals {
-  bucket_name = "serverless"
+  bucket_name         = "serverless"
   default_root_object = "index.html"
 }
 
 module "s3" {
   source = "../../modules/s3"
-  
+
   bucket_name = local.bucket_name
 }
 
 module "cloudfront" {
   source = "../../modules/cloudfront"
-  
-  origin_s3_bucket = local.bucket_name
+
+  origin_s3_bucket    = local.bucket_name
   default_root_object = local.default_root_object
 }
