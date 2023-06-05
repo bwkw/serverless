ce9845b35ff6ed3a75ff77ec3a78846e8afcc7ca
bwkw <na.shota.1139@gmail.com>
2023-06-05 07:27:20 -0700

    :sparkles: Output bucket info for CloudFront

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -1,8 +1,4 @@
 resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
-}
-
-resource "aws_s3_bucket_acl" "main" {
-  bucket = aws_s3_bucket.main.id
   acl    = "private"
 }
--- infra/modules/s3/output.tf
+++ infra/modules/s3/output.tf
@@ -2,3 +2,13 @@ output "bucket_name" {
   description = "The name of the S3 bucket"
   value       = var.bucket_name
 }
+
+output "bucket_id" {
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.main.id
+}
+
+output "bucket_domain_name" {
+  description = "The domain name of the bucket"
+  value       = aws_s3_bucket.main.bucket_domain_name
+}
